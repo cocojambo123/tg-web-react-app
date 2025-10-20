6bf4fdd0b70aeeaa00b343b7bb7d435e6e472f40
Vlad <vgalagush@gmail.com>
2025-10-20 14:48:43 +0300

    qwerttoeo

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ProductList from "./components/ProductList/ProductList";
 import Form from "./components/Form/Form";
 
 function App() {
-  const { tg, onToggleButton } = useTelegram();
+  const { tg } = useTelegram();
   useEffect(() => {
     if (tg && tg.ready) {
       tg.ready();

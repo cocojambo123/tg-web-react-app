7d769e8a33e79c8d8f9f20e7e41f10c7e9c918ed
Vlad <vgalagush@gmail.com>
2025-10-20 13:13:49 +0300

    fsf

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect } from "react";
 import "./App.css";
 import Header from "./components/Header/Header";
 import { useTelegram } from "./hooks/useTelegram";
-const tg = window.Telegram.WebApp;
 
 function App() {
   const { tg, onToggleButton } = useTelegram();
